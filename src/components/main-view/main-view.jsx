bbec7eeb791cc3d7fb31670423a60e5caa2f3cbd
MarcinBiesiada <mtbiesiada@gmail.com>
2025-07-17 19:06:05 +0200

    LoginVIew and SignupView, User and Token stored in local storage
    If applied, this commit would have added two views: LoginView and SignupView and stored user and token in local storage.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,44 +1,99 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Vertigo",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/7/75/Vertigomovie_restoration.jpg",
-      author: "Alfred Hitchcock"
-    },
-    {
-      id: 2,
-      title: "Rope",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/8/8e/Rope2.jpg",
-      author: "Alfred Hitchcock"
-    },
-    {
-      id: 3,
-      title: "All About My Mother",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/d/d7/All_about_my_mother.jpg",
-      author: "Pedro Almodovar"
-    },
-  ]);
-
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+
+  useEffect(() => {
+    if (!token) return;
+
+    fetch("https://mymyflixapp-46a281636c8c.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            id: movie._id?.$oid || movie._id,
+            title: movie.Title,
+            image: movie.ImagePath,
+            director: movie.Director?.Name
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        <p className="separator">or</p>
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
-    return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
+    return (
+      <>
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear();
+          }}
+        >
+          Logout
+        </button>
+        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      </>
+    );
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  } 
+    return (
+      <>
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear();
+          }}
+        >
+          Logout
+        </button>
+        <div>The list is empty!</div>
+      </>
+    );
+  }
 
   return (
     <div>
+      <button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }}
+      >
+        Logout
+      </button>
+
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
@@ -50,4 +105,4 @@ export const MainView = () => {
       ))}
     </div>
   );
-  }
+};
